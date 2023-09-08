c96b381ad1268d78cd9d9adbb21e0402afe777f1
Mislene Silva Moura <mislene.moura2000@gmail.com>
2023-09-08 13:51:59 -0300

    Realizado teste entrevista

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ const pessoaEstudante = {
     cpf: 91349341037, 
     modulo: modulosDoCurso[1]
 }
+
+//listaDePessoasEstudantes = pessoaEstudante;
+
+let novaListaDeEstudante = listaDePessoasEstudantes.push(pessoaEstudante);
+console.log(novaListaDeEstudante)
+console.log('Mislene');
+
+pessoaEstudante.nome = 'Mislene';
+pessoaEstudante.idade = 23;
+pessoaEstudante.cpf = 91349341037;
+pessoaEstudante.modulo = modulosDoCurso[0];
+
+
+let novaVariavel = listaDePessoasEstudantes.length;
+console.log(novaVariavel);
+
+let novaPessoaEstudante = {
+    ...pessoaEstudante, 
+    nome: 'Mislene',
+    idade: 23,
+    cpf: 1234567893,
+    modulo: modulosDoCurso[0]
+}
+
+listaDePessoasEstudantes.push(novaPessoaEstudante);
+console.log(listaDePessoasEstudantes);
+
+const mapeamentoPessoa = novaPessoaEstudante.map( (e) => e.idade +1)
+console.log(mapeamentoPessoa);
